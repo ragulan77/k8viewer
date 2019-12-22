e15ea8bb760080e79cd90f3937f4ecfadb42ec3a
Ragulan Charlesm <ragulan.charlesm@gmail.com>
2019-12-22 17:49:27 +0100

    first commit off the backend project

---
+++ backend/app.js
@@ -0,0 +1,44 @@
+//express constants
+const express = require("express");
+const app = express();
+
+//k8s client constants
+const Client = require("kubernetes-client").Client;
+const config = require("kubernetes-client/backends/request").config;
+
+const client = new Client({ version: "1.13" });
+
+app.get("/", (req, res) => {
+  client.api.v1.namespaces.get().then(kres => {
+    res.send(kres);
+  });
+});
+
+app.get("/getPods", (req, res) => {
+  client.api.v1
+    .namespaces("default")
+    .pods()
+    .get()
+    .then(kres => {
+      res.send(kres);
+    });
+});
+
+app.get("/getNodes", (req, res) => {
+  client.api.v1
+    .nodes()
+    .get()
+    .then(kres => {
+      res.send(kres);
+    });
+});
+
+app.get("/getDeployments", (req, res) => {
+  client.apis.apps.v1.deployments.get().then(kres => {
+    res.send(kres);
+  });
+});
+
+app.listen(5000, () => {
+  console.log("Application listening on port 5000");
+});
