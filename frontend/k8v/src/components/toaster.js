811be64ca1739ad661c26678f281f2d9481c553f
Ragulan Charlesm <ragulan.charlesm@gmail.com>
2019-12-29 12:05:00 +0100

    new helper functions for using the toaster easily

--- frontend/k8v/src/components/toaster.js
+++ frontend/k8v/src/components/toaster.js
@@ -1,6 +1,22 @@
-import { Position, Toaster, Classes } from "@blueprintjs/core";
+import { Position, Toaster, Intent } from "@blueprintjs/core";
 
 export const MyToaster = Toaster.create({
   className: "recipe-toaster",
   position: Position.TOP
 });
+
+export const toasterSuccessMsg = message => {
+  MyToaster.show({
+    message: message,
+    icon: "tick",
+    intent: Intent.SUCCESS
+  });
+};
+
+export const toasterErrorMsg = message => {
+  MyToaster.show({
+    message: message,
+    icon: "warning-sign",
+    intent: Intent.DANGER
+  });
+};
